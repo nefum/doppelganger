f9a82b3499e84aa3cd53fedee7b8615f3b4e3d93
Parker Wahle <regulad@regulad.xyz>
2024-07-06 22:10:00 -0400

    sentry

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,9 +17,11 @@ export const config = {
      * - jsmpeg (JSMPEG paths; WS)
      * - events (Events paths; accessed directly via a WS by the Redroid server)
      *
+     * - monitoring (Sentry monitoring paths)
+     *
      * - Any file extensions like .svg, .png, .jpg, .jpeg, .gif, .webp
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|.*kasmvnc$|.*jsmpeg$|.*events$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|monitoring|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|.*kasmvnc$|.*jsmpeg$|.*events$).*)",
   ],
 };
