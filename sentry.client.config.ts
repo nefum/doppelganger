7984741400e459012ded54655085f8c5bde72dd8
Parker Wahle <regulad@regulad.xyz>
2024-07-17 13:23:35 -0400

    disable sentry screenshots

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -30,6 +30,7 @@ Sentry.init({
     Sentry.feedbackIntegration({
       // Additional SDK configuration goes in here, for example:
       colorScheme: "system",
+      enableScreenshot: false,
     }),
   ],
 });
