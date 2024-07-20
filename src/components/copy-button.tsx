d28ce16dd5d4b4a134a8e6741e2e73613ea32d4f
Parker Wahle <regulad@regulad.xyz>
2024-07-20 19:45:23 -0400

    fomatting & feedback

--- src/components/copy-button.tsx
+++ src/components/copy-button.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button.tsx";
+import { useToast } from "@/components/ui/use-toast.ts";
 import { LuCopy } from "react-icons/lu";
 
 export default function CopyButton({
@@ -10,10 +11,25 @@ export default function CopyButton({
   value: string;
   className?: string;
 }) {
+  const { toast } = useToast();
   return (
     <Button
       variant="ghost"
-      onClick={() => navigator.clipboard.writeText(value)}
+      onClick={() =>
+        navigator.clipboard
+          .writeText(value)
+          .then(() =>
+            toast({
+              title: "Copied successfully!",
+            }),
+          )
+          .catch(() =>
+            toast({
+              title: "Failed to copy",
+              description: "Please try again later.",
+            }),
+          )
+      }
     >
       <LuCopy className={className} />
     </Button>
