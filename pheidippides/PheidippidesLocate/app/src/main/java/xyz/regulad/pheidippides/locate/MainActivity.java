dcac5dd71c034e6ae055e821efd16fed81d3d4af
Parker Wahle <regulad@regulad.xyz>
2024-07-29 12:01:25 -0400

    create locate package

---
+++ pheidippides/PheidippidesLocate/app/src/main/java/xyz/regulad/pheidippides/locate/MainActivity.java
@@ -0,0 +1,7 @@
+package xyz.regulad.pheidippides.locate;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+}
---
+++ pheidippides/PheidippidesLocate/app/src/main/java/xyz/regulad/pheidippides/locate/util/DoppelgangerUtil.java
@@ -0,0 +1,26 @@
+package xyz.regulad.pheidippides.locate.util;
+
+import org.jetbrains.annotations.NotNull;
+
+public class DoppelgangerUtil {
+    public static final @NotNull String CUSTOM_HEADER_NAME = "X-Doppelganger-Secret";
+    public static @NotNull String DOPPELGANGER_ORIGIN;
+    public static @NotNull String DOPPELGANGER_SECRET;
+    public static @NotNull String DOPPELGANGER_DEVICE_ID;
+
+    static {
+        DOPPELGANGER_ORIGIN = SystemPropertyHelper.getSystemProperty("persist.doppelganger.origin", "https://doppelgangerhq.com");
+
+        DOPPELGANGER_SECRET = SystemPropertyHelper.getSystemProperty("persist.doppelganger.secret", "badCanary");
+
+        if (DOPPELGANGER_SECRET.equals("badCanary")) {
+            throw new IllegalStateException("Doppelganger secret not set");
+        }
+
+        DOPPELGANGER_DEVICE_ID = SystemPropertyHelper.getSystemProperty("persist.doppelganger.device", "badCanary");
+
+        if (DOPPELGANGER_DEVICE_ID.equals("badCanary")) {
+            throw new IllegalStateException("Doppelganger device ID not set");
+        }
+    }
+}
