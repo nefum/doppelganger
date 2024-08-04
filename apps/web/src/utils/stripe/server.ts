9284ed5a88c9cdce7792f4bee719195d1bb71703
Parker Wahle <regulad@regulad.xyz>
2024-08-04 13:03:26 -0400

    Update server.ts

--- apps/web/src/utils/stripe/server.ts
+++ apps/web/src/utils/stripe/server.ts
@@ -1,11 +1,11 @@
 import "server-only";
 import Stripe from "stripe";
 
-if (!process.env.STRIPE_SECRET) {
-  throw new Error("STRIPE_SECRET is not set");
-}
-
 export default async function createClient(): Promise<Stripe> {
+  if (!process.env.STRIPE_SECRET) {
+    throw new Error("STRIPE_SECRET is not set");
+  }
+
   return new Stripe(process.env.STRIPE_SECRET!, {
     apiVersion: "2024-06-20",
     appInfo: {
